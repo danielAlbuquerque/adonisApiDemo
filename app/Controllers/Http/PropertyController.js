c2c70d984b846f7bca74bd0b66d19ef4cadc99f6
Daniel Albuquerque <daniel.alb.querque@gmail.com>
2019-01-07 15:47:45 -0200

    initial_commit

---
+++ app/Controllers/Http/PropertyController.js
@@ -0,0 +1,108 @@
+'use strict'
+
+const Property = use('App/Models/Property')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with properties
+ */
+class PropertyController {
+  /**
+   * Show a list of all properties.
+   * GET properties
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+    const properties = Property.all()
+    return properties
+  }
+
+  /**
+   * Render a form to be used for creating a new property.
+   * GET properties/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new property.
+   * POST properties
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single property.
+   * GET properties/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+    const property = await Property.findOrFail(params.id)
+    await property.load('images')
+    return property
+  }
+
+  /**
+   * Render a form to update an existing property.
+   * GET properties/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update property details.
+   * PUT or PATCH properties/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a property with id.
+   * DELETE properties/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+    const property = await Property.findOrFail(params.id)
+
+    if (property.user_id !== auth.user.id) {
+      const error = 'Not authorized'
+      return response.status(401).send({error})
+    }
+
+    await property.delete()
+  }
+}
+
+module.exports = PropertyController
